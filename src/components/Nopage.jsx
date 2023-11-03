7c4928598c4a0f0192952684c915194515c9023e
mohanraj-sece <mmohanraj632@gmail.com>
2023-11-03 10:50:13 +0530

    404 Error Page is created

--- src/components/Nopage.jsx
+++ src/components/Nopage.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Nopage = () => {
   return (
-    <div>Nopage</div>
+    <div className=' min-h-screen gradient-bg-nopage flex flex-col justify-center items-center'>
+        <div className='mf:text-8xl text-4xl text-white'>404 </div>
+        <div className=' p-4 mf:text-5xl text-2xl text-white'>Page Not Found</div>
+        <div  className=' p-4 mf:text-1xl text-lg text-white'>The resource requested could not be found on this server!</div>
+    </div>
   )
 }
 
-export default Nopage
+export default Nopage;
